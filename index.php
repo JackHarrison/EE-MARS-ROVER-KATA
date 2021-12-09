24eeed952645394d414927381c8a17aefb6aed70
john-tribecms <john@tribecms.com>
2021-12-09 17:34:26 +0000

    small error check

--- index.php
+++ index.php
@@ -2,29 +2,36 @@
 
 $acceptedCommands = ['B','F','L','R'];
 $directions = $_GET['directions'];
-$commands = str_split($directions);
 
-include './Kata.php';
-$rover = new Kata();
+if (isset($_GET['directions']) && $directions != "") {
+    $commands = str_split($directions);
 
-$rover->initialCoords->x = 4;
-$rover->initialCoords->y = 2;
-$rover->initialCoords->direction = 'EAST';
-$rover->initialCoords->heading = $rover->directionValues[$rover->initialCoords->direction];
+    include './Kata.php';
+    $rover = new Kata();
 
-foreach ($commands as $command) {
-    echo '<pre>';
-    if (in_array($command, $acceptedCommands)) {
-        $rover->getNewPosition($command);
+    $rover->initialCoords->x = 4;
+    $rover->initialCoords->y = 2;
+    $rover->initialCoords->direction = 'EAST';
+    $rover->initialCoords->heading = $rover->directionValues[$rover->initialCoords->direction];
+
+    foreach ($commands as $command) {
         echo '<pre>';
-        echo 'ROVER IS FACING: ' . $rover->updatedCoords->direction . '<br>';
-        echo 'ROVER IS AT: X:' . $rover->updatedCoords->x . ' Y:' . $rover->updatedCoords->y . '<br>';
-        echo 'HEADING: ' . $rover->updatedCoords->heading . '&deg;' . '<br>';
-        echo '<hr>';
+        if (in_array($command, $acceptedCommands)) {
+            $rover->getNewPosition($command);
+            echo '<pre>';
+            echo 'ROVER IS FACING: ' . $rover->updatedCoords->direction . '<br>';
+            echo 'ROVER IS AT: X:' . $rover->updatedCoords->x . ' Y:' . $rover->updatedCoords->y . '<br>';
+            echo 'HEADING: ' . $rover->updatedCoords->heading . '&deg;' . '<br>';
+            echo '<hr>';
 
-    } else {
-        echo 'UNHANDLED COMMAND STRING';
-        echo '<hr>';
+        } else {
+            echo 'UNHANDLED COMMAND STRING. COMMAND "' . $command . '" NOT KNOWN';
+            echo '<hr>';
+        }
+        echo '</pre>';
     }
+} else {
+    echo '<pre>';
+    echo "no commands found";
     echo '</pre>';
 }
