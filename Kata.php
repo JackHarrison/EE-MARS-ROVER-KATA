decd0e21737dd7121b4ba84a76edefdd4a3a057c
john-tribecms <john@tribecms.com>
2021-12-16 11:58:35 +0000

    fixed huge stupid bug

--- Kata.php
+++ Kata.php
@@ -4,6 +4,7 @@ class Kata
 {
 
     public array $directionValues = ['EAST' => 270, 'WEST' => 90, 'NORTH' => 360, 'SOUTH' => 180];
+    public array $acceptedCommands = ['B','F','L','R'];
     private int $maxHeading = 360;
     private int $minHeading = 0;
 
@@ -11,60 +12,110 @@ class Kata
     public object $updatedCoords;
 
 
+    /**
+     * @param $command
+     */
     public function getNewPosition($command)
     {
         $this->updatedCoords = $this->initialCoords;
 
         switch ($command) {
             case 'F':
-                $this->changePositionalValueDependingOnDirection(1);
+                $this->changePositionalValueDependingOnDirection($this->updatedCoords->direction, 1);
                 break;
             case 'B':
-                $this->changePositionalValueDependingOnDirection(-1);
+                $this->changePositionalValueDependingOnDirection($this->updatedCoords->direction, -1);
                 break;
             case 'L':
-
-                if ($this->updatedCoords->heading == 90) {
-                    $this->updatedCoords->heading = $this->maxHeading;
-                } else {
-                    $this->updatedCoords->heading -= 90;
-                }
-
+            case 'R':
+                $this->updatedCoords->heading = $this->fixHeading($command, $this->updatedCoords->heading);
                 $this->updatedCoords->direction = $this->changeDirectionStringDependingOnHeading();
                 break;
+        }
+
+    }
+
+
+
+    /**
+     * @param $direction
+     * fixes  the issue that 0 degrees is unhandled - I convert this to 360 for abs north
+     */
+    public function fixHeading($direction, $heading)
+    {
+        switch ($direction){
+            case 'L':
+
+                if ($heading == 90) {
+                    return $this->maxHeading;
+                } else {
+                    return $heading -= 90;
+                }
             case 'R':
 
-                if ($this->updatedCoords->heading == $this->maxHeading) {
-                    $this->updatedCoords->heading = $this->minHeading;
+                if ($heading == $this->maxHeading) {
+                    return $this->minHeading;
                 }
-
-                $this->updatedCoords->heading += 90;
-                $this->updatedCoords->direction = $this->changeDirectionStringDependingOnHeading();
-                break;
+                return $heading += 90;
         }
     }
 
 
-    private function changePositionalValueDependingOnDirection($value)
+    /**
+     * @param $value
+     */
+    public function changePositionalValueDependingOnDirection($direction, $value)
     {
-
-        switch ($this->updatedCoords->direction) {
-            case 'WEST':
-            case 'EAST':
-            $this->updatedCoords->x += $value;
-                break;
-            case 'NORTH':
-            case 'SOUTH':
-            $this->updatedCoords->y += $value;
-                break;
+        if ($direction === 'EAST') {
+            $this->updatedCoords->x -= $value;
         }
+
+        if ($direction === 'WEST') {
+            $this->updatedCoords->x += $value;
+        }
+
+        if ($direction === 'NORTH') {
+            $this->updatedCoords->y += $value;
+        }
+
+        if ($direction === 'SOUTH') {
+            $this->updatedCoords->y -= $value;
+        }
+
     }
 
 
-    private function changeDirectionStringDependingOnHeading()
+    /**
+     * @return false|int|string
+     */
+    public function changeDirectionStringDependingOnHeading()
     {
         return array_search($this->updatedCoords->heading, $this->directionValues);
     }
 
+
+    /**
+     * @param $result // success, no command received or an unknown command
+     */
+    public function printResults ($result) {
+        echo '<pre>';
+        switch ($result){
+            case 'success':
+                echo 'ROVER IS FACING: ' . $this->updatedCoords->direction . '<br>';
+                echo 'ROVER IS AT: X:' . $this->updatedCoords->x . ' Y:' . $this->updatedCoords->y . '<br>';
+                echo 'HEADING: ' . $this->updatedCoords->heading . '&deg;' . '<br>';
+                echo '<hr>';
+                break;
+            case 'nocommand':
+                echo "NO COMMAND RECEIVED";
+                echo '<hr>';
+                break;
+            default:
+                echo 'UNHANDLED COMMAND STRING. COMMAND "' . $result . '" NOT KNOWN';
+                echo '<hr>';
+                break;
+        }
+        echo '</pre>';
+    }
 
 }
--- index.php
+++ index.php
@@ -1,37 +1,27 @@
 <?php
 
-$acceptedCommands = ['B','F','L','R'];
 $directions = $_GET['directions'];
+include './Kata.php';
+$rover = new Kata();
 
 if (isset($_GET['directions']) && $directions != "") {
     $commands = str_split($directions);
 
-    include './Kata.php';
-    $rover = new Kata();
-
-    $rover->initialCoords->x = 4;
-    $rover->initialCoords->y = 2;
-    $rover->initialCoords->direction = 'EAST';
+    $rover->initialCoords->x = 0;
+    $rover->initialCoords->y = 0;
+    $rover->initialCoords->direction = 'WEST';
     $rover->initialCoords->heading = $rover->directionValues[$rover->initialCoords->direction];
 
     foreach ($commands as $command) {
-        echo '<pre>';
-        if (in_array($command, $acceptedCommands)) {
+        if (in_array($command, $rover->acceptedCommands)) {
             $rover->getNewPosition($command);
-            echo '<pre>';
-            echo 'ROVER IS FACING: ' . $rover->updatedCoords->direction . '<br>';
-            echo 'ROVER IS AT: X:' . $rover->updatedCoords->x . ' Y:' . $rover->updatedCoords->y . '<br>';
-            echo 'HEADING: ' . $rover->updatedCoords->heading . '&deg;' . '<br>';
-            echo '<hr>';
-
+            $rover->printResults('success');
         } else {
-            echo 'UNHANDLED COMMAND STRING. COMMAND "' . $command . '" NOT KNOWN';
-            echo '<hr>';
+            $rover->printResults($command);
         }
-        echo '</pre>';
     }
 } else {
-    echo '<pre>';
-    echo "no commands found";
-    echo '</pre>';
+    $rover->printResults('nocommand');
 }
+
+
